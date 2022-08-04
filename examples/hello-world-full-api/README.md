eb20953ddd4e8d503c0a543e1ed05e04e779b126
Olawale Malomo <warlay234@gmail.com>
2022-08-04 17:35:56 +0100

    Add v1 of open widget library

---
+++ examples/hello-world-full-api/README.md
@@ -0,0 +1,8 @@
+# Setup
+* `git clone https://github.com/Changers/SDKWidgets`
+* `cd examples/hello-world-full-api`
+* `npm install`
+* `npm install http-server -g`
+* `http-server -p 8082 ./`
+
+# Test
---
+++ examples/hello-world-full-api/hello-world.js
@@ -0,0 +1,101 @@
+'use strict';
+
+(function() {
+  class HelloWorld extends HTMLElement {
+    constructor() {
+      // establish prototype chain
+      super();
+
+      // attaches shadow tree and returns shadow root reference
+      // https://developer.mozilla.org/en-US/docs/Web/API/Element/attachShadow
+      const shadow = this.attachShadow({ mode: 'open' });
+
+      this._user = {};
+      this._api = {};
+      this._counter = 0;
+
+      // creating a container for the editable-list component
+      const container = document.createElement('div');
+
+      // creating the inner HTML of the editable list element
+      container.innerHTML = `
+        <h2>Hello, User #<span class="user-id">${this.userId}</span>!</h2>
+        <h5>Balance: <span class="balance">${this.balance}</span></h5>
+        <h5>Counter: <span class="counter">${this.counter}</span></h5>
+        <button class="increment">increment</button>
+        <button class="open-full-screen">Open full screen</button>
+        <button class="close-full-screen">Close full screen</button>
+      `;
+
+      // appending the container to the shadow DOM
+      shadow.appendChild(container);
+    }
+
+    // fires after the element has been attached to the DOM
+    connectedCallback() {
+      console.log("connectedCallback", this.shadowRoot, this, this.user)
+
+      const incrementButton = this.shadowRoot.querySelector('.increment');
+
+      let self = this;
+
+      incrementButton.addEventListener('click', function(e) {
+        console.log("addEventListener", self.shadowRoot, self, self.user)
+
+        self._counter ++;
+
+        self.update()
+      }, false);
+
+      const openFullScreenButton = this.shadowRoot.querySelector('.open-full-screen');
+      openFullScreenButton.addEventListener('click', () => self.api.openFullScreen())
+
+      const closeFullScreenButton = this.shadowRoot.querySelector('.close-full-screen');
+      closeFullScreenButton.addEventListener('click', () => self.api.closeFullScreen())
+    }
+
+    update() {
+      var counterElement = this.shadowRoot.querySelector('.counter');
+      counterElement.innerHTML = this.counter;
+
+      var userIdElement = this.shadowRoot.querySelector('.user-id');
+      userIdElement.innerHTML = this.userId;
+
+      var balanceElement = this.shadowRoot.querySelector('.balance');
+      balanceElement.innerHTML = this.balance;
+    }
+
+    set user(value) {
+      this._user = value;
+      this.update();
+    }
+
+    get user() {
+      return this._user;
+    }
+
+    set api(value) {
+      this._api = value;
+      this.update();
+    }
+
+    get api() {
+      return this._api;
+    }
+
+    get userId() {
+      return this._user.id;
+    }
+
+    get balance() {
+      return this._user.coins;
+    }
+
+    get counter() {
+      return this._counter;
+    }
+  }
+
+  // let the browser know about the custom element
+  customElements.define('hello-world', HelloWorld);
+})();
