b177e438b8c2f0b2493c68e7f165d6a44fc00a76
Olawale Malomo <warlay234@gmail.com>
2022-09-16 09:30:04 +0100

    Add new widget opening and title update

--- examples/hello-world-lit-js/widget.js
+++ examples/hello-world-lit-js/widget.js
@@ -1,3 +1,13 @@
-import { HelloWorld } from './src/HelloWorld.js';
+import {HelloWorld} from './src/HelloWorld.js';
+
+class HelloWorldNew extends HelloWorld {
+  widgetTitle = 'Hello world new';
+  newWidgetName = 'hello-world';
+
+  constructor() {
+    super();
+  }
+}
 
 window.customElements.define('hello-world', HelloWorld);
+window.customElements.define('hello-world-new', HelloWorldNew);
